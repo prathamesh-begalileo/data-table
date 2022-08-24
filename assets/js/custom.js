fe696339bc562b614c9a15d56001ec7031bbf0f5
prathamesh <prathamesh@begalileo.com>
2022-08-24 10:22:05 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  var table = $("#example").DataTable({
+    buttons: ["copy", "csv", "excel", "pdf", "print"],
+  });
+
+  table.buttons().container().appendTo("#example_wrapper .col-md-6:eq(0)");
+});
